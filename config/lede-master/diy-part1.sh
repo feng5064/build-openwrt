0caf35e1ff1ee42c8fffb52bebf96e925bcb5da7
feng5064 <pazu@live.com>
2023-08-22 11:40:27 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,10 +8,6 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages'>> feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci'>> feeds.conf.default
-echo 'src-git routing https://github.com/coolsnowwolf/routing'>> feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git'>> feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
